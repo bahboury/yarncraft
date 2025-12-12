a6778aa411f2f48c0c10d76dded53bda4fbe4ea0
Eslam-AhmedH <dev.islam.ahmed@gmail.com>
2025-12-12 09:19:59 +0200

    Implement Product Module

--- src/main/java/com/swe2project/yarncraft/modules/product/dto/ProductRequest.java
+++ src/main/java/com/swe2project/yarncraft/modules/product/dto/ProductRequest.java
@@ -1,4 +1,16 @@
 package com.swe2project.yarncraft.modules.product.dto;
 
+import com.swe2project.yarncraft.modules.product.entity.Category;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class ProductRequest {
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Category category;
+    private String imageUrl;
 }
+
--- src/main/java/com/swe2project/yarncraft/modules/product/entity/Category.java
+++ src/main/java/com/swe2project/yarncraft/modules/product/entity/Category.java
@@ -1,4 +1,12 @@
 package com.swe2project.yarncraft.modules.product.entity;
 
 public enum Category {
+    YARN,
+    CROCHET_HOOKS,
+    KNITTING_NEEDLES,
+    PATTERNS,
+    BAGS,
+    CLOTHING,
+    ACCESSORIES,
+    HOME_DECOR
 }
